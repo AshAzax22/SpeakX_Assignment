e839f9e4961e42e3f3942c10139da0f2b065e3ec
Ashutosh <ashutoshash.az22@gmail.com>
2025-01-26 21:03:04 +0530

    Api fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const getQuestions = async (title, page, limit) => {
   try {
     const response = await fetch(
-      `http://localhost:3000/questions?title=${title}&page=${page}&limit=${limit}`,
+      `${API_BASE_URL}/questions?title=${title}&page=${page}&limit=${limit}`,
       {
         methid: "GET",
         headers: {
